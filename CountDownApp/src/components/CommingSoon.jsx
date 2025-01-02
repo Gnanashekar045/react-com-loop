f59ab64fced2510be84c835fe038145008c8ec35
Gnanashekar045 <gnanashekar045@gmail.com>
2025-01-02 22:58:12 +0530

    Update CommingSoon.jsx

--- CountDownApp/src/components/CommingSoon.jsx
+++ CountDownApp/src/components/CommingSoon.jsx
@@ -7,7 +7,7 @@ const ComingSoon = () => {
         minutes: '00',
         seconds: '00'
     });
-//useEffect to rendering
+//useEffect to 
     useEffect(() => {
         const countDownDate = new Date("Nov 17, 2024 00:00:00").getTime();
 
